c86e4883fb23b10f4124e02b25aefafbe8fc910f
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-10-30 17:56:08 +0530

    remove

--- App.js
+++ App.js
@@ -69,31 +69,4 @@ alert(JSON.stringify(err));
     );
   }
 /////check system is running or not
-export default App;
-import * as React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>hello World</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <HomeScreen/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
